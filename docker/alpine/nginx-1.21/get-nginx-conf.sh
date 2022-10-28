81de31d51666f4a1efe3aadfc759bb37bd681c07
jingjingxyk <zonghengbaihe521@qq.com>
2022-10-28 13:03:00 +0800

    修改构建Dockerfile 目录配置

--- docker/alpine/nginx-1.21/get-nginx-conf.sh
+++ docker/alpine/nginx-1.21/get-nginx-conf.sh
@@ -4,9 +4,27 @@ set -eux
 __DIR__=$(cd "$(dirname "$0")";pwd)
 cd ${__DIR__}
 
-image=nginx:alpine
-image=jingjingxyk/nginx:alpine-1.21.1-connect-proxy-20220811T2356Z
-container_id=$(docker create $image)  # returns container ID
-docker cp $container_id:/usr/local/nginx/nginx.conf nginx-default.conf
 
+# 获取最新容器tag
+container_latest_tag=$(curl -L -s 'https://hub.docker.com/v2/repositories/jingjingxyk/nginx/tags/?page_size=1&page=1&ordering=last_updated'|jq '."results"[]["name"]'| sed 's/\"//g')
+echo $container_latest_tag
+
+image="docker.io/jingjingxyk/nginx:$container_latest_tag"
+
+docker pull $image
+
+container_id=$(docker create $image)  # returns container ID
+docker cp $container_id:/usr/local/nginx/etc/nginx.conf nginx-default.conf
+docker cp $container_id:/usr/local/nginx/etc/conf.d/default.conf nginx-default-domain.conf
+
+docker rm $container_id
+
+
+
+exit 0
+
+image=nginx:alpine
+container_id=$(docker create $image)  # returns container ID
+docker cp $container_id:/etc/nginx/nginx.conf nginx-default.conf
+docker cp $container_id:/etc/nginx/conf.d/default.conf nginx-default-domain.conf
 docker rm $container_id
